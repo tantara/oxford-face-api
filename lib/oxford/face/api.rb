0d9788ee956b58b134867327e4c2e39feb2a1f18
Taekmin Kim <tantara.tm@gmail.com>
2017-01-03 20:31:50 +0900

    test image

--- lib/oxford/face/api.rb
+++ lib/oxford/face/api.rb
@@ -1,9 +1,278 @@
 require "oxford/face/api/version"
+require "oxford/face/http_client"
 
-module Oxford
-  module Face
-    module Api
-      # Your code goes here...
-    end
+module OxfordFaceApi
+  @api_key = ""
+
+  def self.init(api_key)
+	@api_key = api_key
+  end
+
+  def self.API_KEY
+	@api_key
+  end
+
+  class Face
+	def initialize
+	  raise if OxfordFaceApi.API_KEY.empty?
+	  @client = HttpClient.new(OxfordFaceApi.API_KEY, true)
+	end
+
+	# Detect
+	# POST https://api.projectoxford.ai/face/v1.0/detect[?returnFaceId][&returnFaceLandmarks][&returnFaceAttributes]
+	def detect(url, return_face_id = true, return_face_landmarks = false, return_face_attributes = "")
+	  params = {
+		url: url
+	  }
+	  @client.post("/detect?returnFaceId=#{return_face_id}&returnFaceLandmarks=#{return_face_landmarks}&returnFaceAttributes=#{return_face_attributes}", params)
+	end
+
+	# TODO
+	# Find Similar
+	# POST https://api.projectoxford.ai/face/v1.0/findsimilars
+	def find_similar
+	  params = {
+	  }
+	  @client.post("/findsimilars", params)
+	end
+
+	# TODO
+	# Group
+	# POST https://api.projectoxford.ai/face/v1.0/group
+	def group
+	  params = {
+	  }
+	  @client.post("/group", params)
+	end
+
+	# Identify
+	# POST https://api.projectoxford.ai/face/v1.0/identify
+	def identify(group_id, face_ids, max_num_of_candidates_returned = 1, confidence_threshold = 0.5)
+	  params = {
+		faceIds: face_ids,
+		personGroupId: group_id, 
+		maxNumOfCandidatesReturned: max_num_of_candidates_returned,
+		confidenceThreshold: confidence_threshold
+	  }
+	  @client.post("/identify", params)
+	end
+
+	# Verify
+	# POST https://api.projectoxford.ai/face/v1.0/verify
+	def verify(face_id, group_id, person_id = "")
+	  if person_id.empty?
+		face_id1 = face_id
+		face_id2 = group_id
+
+		params = {
+		  faceId1: face_id1,
+		  faceId2: face_id2,
+		}
+	  else
+		params = {
+		  faceId: face_id,
+		  personGroupId: group_id,
+		  personId: person_id
+		}
+	  end
+	  @client.post("/verify", params)
+	end
+  end
+
+  class FaceList
+	def initialize
+	  raise if OxfordFaceApi.API_KEY.empty?
+	  @client = HttpClient.new(OxfordFaceApi.API_KEY)
+	end
+
+	# Add a Face to a Face List
+	# POST https://api.projectoxford.ai/face/v1.0/facelists/{faceListId}/persistedFaces[?userData][&targetFace]
+	def add_face(face_list_id, url, user_data = "", target_face = "")
+	  params = {
+		url: url
+	  }
+	  @client.post("/facelists/#{face_list_id}/persistedFaces?userData=#{user_data}&targetFace=#{target_face}", params)
+	end
+
+	# Create a Face List
+	# PUT https://api.projectoxford.ai/face/v1.0/facelists/{faceListId}
+	def create(face_list_id, name, user_data = "")
+	  params = {
+		name: name,
+		userData: user_data
+	  }
+	  @client.put("/facelists/#{face_list_id}", params)
+	end
+
+	# FIXME
+	# Delete a Face from a Face List
+	# DELETE https://api.projectoxford.ai/face/v1.0/facelists/{faceListId}/persistedFaces/{persistedFaceId}
+	def delete_face(face_list_id, face_id)
+	  @client.delete("/facelists/#{face_list_id}/persistedFaces/#{face_id}")
+	end
+
+	# FIXME
+	# Delete a Face List
+	# DELETE https://api.projectoxford.ai/face/v1.0/facelists/{faceListId}
+	def delete(face_list_id)
+	  @client.delete("/facelists/#{face_list_id}")
+	end
+
+	# Get a Face List
+	# GET https://api.projectoxford.ai/face/v1.0/facelists/{faceListId}
+	def faces(face_list_id)
+	  @client.get("/facelists/#{face_list_id}")
+	end
+
+	# List Face Lists
+	# GET https://api.projectoxford.ai/face/v1.0/facelists
+	def all
+	  @client.get("/facelists")
+	end
+
+	# Update a Face List
+	# PATCH https://api.projectoxford.ai/face/v1.0/facelists/{faceListId}
+	def update(face_list_id, name, user_data)
+	  params = {
+		name: name,
+		userData: user_data
+	  }
+	  @client.patch("/facelists/#{face_list_id}", params)
+	end
+  end
+
+  class Person
+	def initialize
+	  raise if OxfordFaceApi.API_KEY.empty?
+	  @client = HttpClient.new(OxfordFaceApi.API_KEY)
+	end
+
+	# Add a Person Face
+	# POST https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons/{personId}/persistedFaces[?userData][&targetFace]
+	def add_face(group_id, person_id, url, user_data = "", target_face = "")
+	  params = {
+		url: url
+	  }
+	  @client.post("/persongroups/#{group_id}/persons/#{person_id}/persistedFaces?userData=#{user_data}&targetFace=#{target_face}", params)
+	end
+
+	# Create a Person
+	# POST https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons
+	def create(group_id, name, user_data = "")
+	  params = {
+		name: name,
+		userData: user_data
+	  }
+	  @client.post("/persongroups/#{group_id}/persons", params)
+	end
+
+	# FIXME
+	# Delete a Person
+	# DELETE https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons/{personId}
+	def delete(group_id, person_id)
+	  @client.delete("/persongroups/#{group_id}/persons/#{person_id}")
+	end
+
+	# FIXME
+	# Delete a Person Face
+	# DELETE https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons/{personId}/persistedFaces/{persistedFaceId}
+	def delete_face(group_id, person_id, face_id)
+	  @client.delete("/persongroups/#{group_id}/persons/#{person_id}/persistedFaces/#{face_id}")
+	end
+
+	# Get a Person
+	# GET https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons/{personId}
+	def get(group_id, person_id)
+	  @client.get("/persongroups/#{group_id}/persons/#{person_id}")
+	end
+
+	# Get a Person Face
+	# GET https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons/{personId}/persistedFaces/{persistedFaceId}
+	def face(group_id, person_id, face_id)
+	  @client.get("/persongroups/#{group_id}/persons/#{person_id}/persistedFaces/#{face_id}")
+	end
+
+	# List Persons in a Person Group
+	# GET https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons
+	def all(group_id)
+	  @client.get("/persongroups/#{group_id}/persons")
+	end
+
+	# Update a Person
+	# PATCH https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons/{personId}
+	def update(group_id, person_id, name, user_data)
+	  params = {
+		name: name,
+		userData: user_data
+	  }
+	  @client.patch("/persongroups/#{group_id}/persons/#{person_id}", params)
+	end
+
+	# Update a Person Face
+	# PATCH ttps://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/persons/{personId}/persistedFaces/{persistedFaceId}
+	def update_face(group_id, person_id, face_id, user_data)
+	  params = {
+		userData: user_data
+	  }
+	  @client.patch("/persongroups/#{group_id}/persons/#{person_id}/persistedFaces/#{face_id}", params)
+	end
+  end
+
+  class PersonGroup
+	def initialize
+	  raise if OxfordFaceApi.API_KEY.empty?
+	  @client = HttpClient.new(OxfordFaceApi.API_KEY)
+	end
+
+	# Create a Person Group
+	# PUT https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}
+	def create(group_id, name, user_data = "")
+	  params = {
+		name: name,
+		userData: user_data
+	  }
+	  @client.put("/persongroups/#{group_id}", params)
+	end
+
+	# FIXME
+	# Delete a Person Group
+	# DELETE https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}
+	def delete(group_id)
+	  @client.delete("/persongroups/#{group_id}")
+	end
+
+	# Get a Person Group
+	# GET https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}
+	def get(group_id)
+	  @client.get("/persongroups/#{group_id}")
+	end
+
+	# Get Person Group Training Status
+	# GET https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/training
+	def status(group_id)
+	  @client.get("/persongroups/#{group_id}/training")
+	end
+
+	# List Person Groups
+	# GET https://api.projectoxford.ai/face/v1.0/persongroups[?start][&top]
+	def all(start = 1, top = 1000)
+	  @client.get("/persongroups?start=#{start}&top=#{top}")
+	end
+
+	# Train Person Group
+	# POST https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}/train
+	def train(group_id)
+	  @client.post("/persongroups/#{group_id}/train")
+	end
+
+	# Update a Person Group
+	# PATCH https://api.projectoxford.ai/face/v1.0/persongroups/{personGroupId}
+	def update(group_id, name, user_data)
+	  params = {
+		name: name,
+		userData: user_data
+	  }
+	  @client.patch("/persongroups/#{group_id}", params)
+	end
   end
 end
---
+++ test/test_api.rb
@@ -0,0 +1,160 @@
+require 'minitest/autorun'
+require 'oxford/face/api'
+
+KEY=File.open("./test/key.txt", "r").readline().strip
+
+class ApiTest < Minitest::Test
+  def test_detect
+	OxfordFaceApi.init(KEY)
+
+	f = OxfordFaceApi::Face.new
+
+	img_url = "https://raw.githubusercontent.com/tantara/oxford-face-api/master/test/example.jpg"
+	return_face_id = true
+	return_face_landmarks = false
+	return_face_attributes = "age,gender"
+	res = f.detect(img_url, return_face_id, return_face_landmarks, return_face_attributes)
+	puts res.body # https://gist.github.com/tantara/932202b63c5d2008af1b138d73383931
+
+	assert_equal "hello world",
+	  "hello world"
+  end
+end
+
+=begin
+KEY="YOUR_KEY" # FIXME
+
+OxfordFaceApi.init(KEY)
+#puts OxfordFaceApi.API_KEY
+
+f = OxfordFaceApi::Face.new
+fl = OxfordFaceApi::FaceList.new
+p = OxfordFaceApi::Person.new
+pg = OxfordFaceApi::PersonGroup.new
+
+# create person group
+group_id = "deepcheck-dev"
+group_name = "group-deepcheck-dev"
+#res = pg.create(group_id, group_name)
+#puts res.body # ""
+
+# add person to person group
+person_name = "person-taekmin"
+person_user_data = "developer"
+#res = p.create(group_id, person_name, person_user_data)
+#puts res.body # {"personId":"3a9a24c9-9ece-4e6b-b511-84a19131496b"} 1311f360-e822-41f7-804b-d2442e827588
+
+# add face to person
+person_id = "3a9a24c9-9ece-4e6b-b511-84a19131496b"
+img_url = "http://photo.tantara.me/photos/1/814684725262998.jpg"
+img_url = "https://dl.dropbox.com/s/24n033ijd16p8u1/10171853_814684725262998_3929584347521148745_n.jpg"
+#res = p.add_face(group_id, person_id, img_url)
+#puts res.body # {"persistedFaceId":"e98c6f3f-b772-4836-8f73-6661ab32fbe2"} # 56e8afa6-7880-4ed9-9caf-772df773a845
+
+# get person information
+#res = p.get(group_id, person_id)
+#puts res.body
+# {"personId":"3a9a24c9-9ece-4e6b-b511-84a19131496b","persistedFaceIds":["e98c6f3f-b772-4836-8f73-6661ab32fbe2"],"name":"person-taekmin","userData":"developer"}
+
+# detect face
+img_url = "http://photo.tantara.me/photos/1/814684725262998.jpg"
+img_url = "https://dl.dropbox.com/s/8xmys57ncm4l2vo/slack_for_ios_upload.jpg"
+return_face_id = true # default: true
+return_face_landmarks = false # default: false
+return_face_attributes = ""#age,gender" # default: ""
+res = f.detect(img_url, return_face_id, return_face_landmarks, return_face_attributes)
+puts res.body # https://gist.github.com/tantara/932202b63c5d2008af1b138d73383931
+
+face_id = "8ffe65f0-e174-4d57-a6ff-c3570221306c" # face_id from https://gist.github.com/tantara/932202b63c5d2008af1b138d73383931
+face_ids = [
+  face_id
+]
+#res = f.identify(group_id, face_ids)
+#puts res.body # type1: not trained, type2: normal
+
+# type1: not trained
+# {"error":{"code":"PersonGroupNotTrained","message":"Person group 'deepcheck-dev' not trained."}}
+# if you get a error message like above
+# you need to train the person group
+#res = pg.train(group_id)
+#puts res.body # ""
+
+# type2: normal
+#[{"faceId":"8ffe65f0-e174-4d57-a6ff-c3570221306c","candidates":[{"personId":"3a9a24c9-9ece-4e6b-b511-84a19131496b","confidence":1.0}]}]
+
+# get group's training status
+#res = pg.status(group_id)
+#puts res.body # {"status":"succeeded","createdDateTime":"10/15/2016 3:20:34 PM","lastActionDateTime":"10/15/2016 3:20:36 PM","message":null}
+
+# get all person groups
+#res = pg.all
+#puts res.body # [{"personGroupId":"deepcheck-dev","name":"group-deepcheck-dev","userData":null},{"personGroupId":"tester","name":"group1","userData":"user-provided data attached to the person group"}]
+
+# update group's information
+new_group_name = "group-deepcheck-dev-updated"
+new_group_user_data = "A to Z"
+#res = pg.update(group_id, new_group_name, new_group_user_data)
+#puts res.body # ""
+
+# get group information
+#res = pg.get(group_id)
+#puts res.body # {"personGroupId":"deepcheck-dev","name":"group-deepcheck-dev-updated","userData":"A to Z"}
+
+####################
+### ADDITIONAL
+####################
+
+# verify1
+face_id1 = "" #FIXME
+face_id2 = "" #FIXME
+#res = f.verify(face_id1, face_id2)
+#puts res.body # {"isIdentical":true, "confidence":0.9}
+
+# verify2
+#res = f.verify(face_id, group_id, person_id)
+#puts res.body # {"isIdentical":true,"confidence":1.0}
+
+#  update person information
+new_person_name = "new-person-taekmin"
+new_person_user_data = "new developer"
+#res = p.update(group_id, person_id, new_person_name, new_person_user_data)
+#puts res.body # ""
+
+# get all person in group
+#res = p.all(group_id)
+#puts res.body # [{"personId":"3a9a24c9-9ece-4e6b-b511-84a19131496b","persistedFaceIds":["e98c6f3f-b772-4836-8f73-6661ab32fbe2"],"name":"new-person-taekmin","userData":"new developer"}]
+
+# update person face information
+face_id_in_group = "e98c6f3f-b772-4836-8f73-6661ab32fbe2"
+new_face_user_data = "update face information"
+#res = p.update_face(group_id, person_id, face_id_in_group, new_face_user_data)
+#puts res.body # ""
+
+# get person's face
+#res = p.face(group_id, person_id, face_id_in_group)
+#puts res.body # {"persistedFaceId":"e98c6f3f-b772-4836-8f73-6661ab32fbe2","userData":"update face information"}
+
+# create facelist
+face_list_id = "deepcheck-dev"
+face_list_name = "facelist-deepcheck-dev"
+#res = fl.create(face_list_id, face_list_name)
+#puts res.body # ""
+
+# get all facelist
+#res = fl.all
+#puts res.body # [{"faceListId":"deepcheck-dev","name":"facelist-deepcheck-dev","userData":null}]
+
+# add face to face list
+#res = fl.add_face(face_list_id, img_url)
+#puts res.body # {"persistedFaceId":"6828c0c6-49ce-4409-975a-e95430067eca"}
+
+# get all face from facelist
+#res = fl.faces(face_list_id)
+#puts res.body # {"persistedFaces":[{"persistedFaceId":"6828c0c6-49ce-4409-975a-e95430067eca","userData":null}],"faceListId":"deepcheck-dev","name":"facelist-deepcheck-dev","userData":null}
+
+# update facelist
+new_face_list_name = "new-facelist-deepcheck-dev"
+new_face_list_user_data = "update facelist information"
+#res = fl.update(face_list_id, new_face_list_name, new_face_list_user_data)
+#puts res.body # ""
+=end
